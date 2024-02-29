4b872b11490bac6348916f5bab58edb744f8c982
sheema <sheemaparwaz@gmail.com>
2024-02-29 15:58:30 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,19 @@
-/** @type {import('next').NextConfig} */ const nextConfig = { output: "export", // <=== enables static exports reactStrictMode: true, }; module.exports = nextConfig;
+/**original config */
+/**
+ //  @type {import('next').NextConfig} 
+const nextConfig = {
+  reactStrictMode: true,
+};
+export default nextConfig;
+ */
+
+
+const isProd = process.env.NODE_ENV === 'production';
+
+//export const assetPrefix = isProd ? '/MY-APP' : '';
+//export const basePath = isProd ? '/MY-APP' : '';
+//const isProd = process.env.NODE_ENV === 'production';
+
+export const output = 'export';
+export const basePath = isProd ? '/headless_cms_p1' : '';
+export const assetPrefix = isProd ? '/headless_cms_p1' : '';
